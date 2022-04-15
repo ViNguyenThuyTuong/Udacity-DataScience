a0668e0a7635e30800f1e2fe4a5a505f31e05aaa
ViNguyenThuyTuong <97575081+ViNguyenThuyTuong@users.noreply.github.com>
2022-04-15 23:09:13 +0700

    first commit for project4

---
+++ Udacity-DataScience-Project4/app/run.py
@@ -0,0 +1,47 @@
+import json
+import plotly
+import pandas as pd
+
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+
+from flask import Flask
+from flask import render_template, request, jsonify
+from plotly.graph_objs import Bar
+from sklearn.externals import joblib
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+# load data
+#df = pd.read_sql_table('disaster_messages', engine)
+
+# load model
+model = joblib.load('models/classifier.pkl')
+
+
+# web page that handles user query and displays model results
+@app.route('/go')
+def go():
+    # save user input in query
+    query = request.args.get('query', '') 
+
+    # use model to predict classification for query
+    classification_labels = model.predict([query])[0]
+    classification_results = dict(query, classification_labels))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        query=query,
+        classification_result=classification_results
+    )
+
+
+def main():
+    app.run(host='0.0.0.0', port=3001, debug=True)
+
+
+if __name__ == '__main__':
+    main()
